826a213e73d9ccd12d7ccc14972e44c285c07dd0
delder004 <dmelder@gmail.com>
2019-02-19 22:15:34 -0800

    Set up proxy server

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '/../public')));
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
