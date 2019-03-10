11959f27fa541097577aef020a7325b7e5e66d52
John Castberg <jpcastberg@gmail.com>
2019-03-09 19:09:27 -0800

    Implement router in proxy server

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const proxy = require('http-proxy-middleware');
 
 const app = express();
 const PORT = 3000;
@@ -11,6 +12,19 @@ app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
+
+const proxyTable = {
+  '/reviews': 'http://localhost:3004',
+};
+
+const options = {
+  target: '/',
+  router: proxyTable,
+};
+
+const myProxy = proxy(options);
+
+app.use(myProxy);
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
