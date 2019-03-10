bc575fcf7d14bb3dee345059352263bb53adc9ca
John Castberg <jpcastberg@gmail.com>
2019-03-09 17:40:08 -0800

    Remove body parser dependency

--- server/index.js
+++ server/index.js
@@ -1,12 +1,10 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
 const PORT = 3000;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static(path.join(__dirname, '/../public')));
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
