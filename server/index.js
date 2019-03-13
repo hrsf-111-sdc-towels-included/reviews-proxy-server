58b4ed3c0b82718fc8a56062c2336bb865e931db
John Castberg <jpcastberg@gmail.com>
2019-03-13 15:59:22 -0700

    Update proxy server to point to nginx load balancer

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(function(req, res, next) {
   next();
 });
 
-const apiProxy = proxy('/reviews/*', { target: 'http://ec2-18-224-182-68.us-east-2.compute.amazonaws.com' });
+const apiProxy = proxy('/reviews/*', { target: 'http://ec2-18-191-179-140.us-east-2.compute.amazonaws.com' });
 app.use(apiProxy);
 
 app.get('/:homeID', (req, res) => {
